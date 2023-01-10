f91cdec7ce6ec9e53dd67a674918166cf26c5274
Chandragupta Singh <chandragupta.singh4@gmail.com>
2023-01-10 23:14:12 +0530

    envconfig files added for mainet and testnet

--- comdex-1/envConfig.js
+++ comdex-1/envConfig.js
@@ -1,1 +1,32 @@
 
+export const envConfig = {
+    rpc: "https://rpc.comdex.one",
+    rest: "https://rest.comdex.one",
+    chainId: "comdex-1",
+    coinDenom: "CMDX",
+    coinMinimalDenom: "ucmdx",
+    coinDecimals: 6,
+    prefix: "comdex",
+    coinType: 118,
+    chainName: "Comdex",
+    explorerUrlToTx: "",
+    apiUrl: "",
+    comdexStakingUrl: "https://comdex.omniflix.co/stake",
+    webSocketApiUrl: "wss://rpc.comdex.one/websocket",
+  
+    commodo: {
+      appId: 3,
+      cswapAppId: 1,
+      numberOfTopAssets: 3,
+      cswapUrl: "https://app.cswap.one",
+      rewardsUrl: "https://docs.commodo.one/rewards",
+      atomCmdxPoolId: 1,
+    },
+  
+    cSwap: {
+      appId: 1,
+      masterPoolId: 1,
+      networkTag: "mainnet",
+    },
+  };
+  
--- testnet-2/envConfig.js
+++ testnet-2/envConfig.js
@@ -1,1 +1,32 @@
 
+export const envConfig = {
+    rpc: "https://test2-rpc.comdex.one",
+    rest: "https://test2-rest.comdex.one",
+    chainId: "comdex-test2",
+    coinDenom: "CMDX",
+    coinMinimalDenom: "ucmdx",
+    coinDecimals: 6,
+    prefix: "comdex",
+    coinType: 118,
+    chainName: "Comdex Test Chain",
+    explorerUrlToTx: "",
+    apiUrl: "",
+    comdexStakingUrl: "https://comdex.omniflix.co/stake",
+    webSocketApiUrl: "wss://rpc.comdex.one/websocket",
+  
+    commodo: {
+      appId: 3,
+      cswapAppId: 1,
+      numberOfTopAssets: 3,
+      cswapUrl: "https://staging.cswap.one",
+      rewardsUrl: "https://docs.commodo.one/rewards",
+      atomCmdxPoolId: 1,
+    },
+  
+    cSwap: {
+      appId: 1,
+      masterPoolId: 1,
+      networkTag: "testnet2",
+    },
+  };
+  
