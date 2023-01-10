3789de22d87e07d0ed4a33814209facfd432f1a8
srinu391 <srinu@comdex.sg>
2023-01-10 16:21:47 +0530

    Remove unused fields.

--- devnet/envConfig.js
+++ devnet/envConfig.js
@@ -11,11 +11,9 @@ export const envConfig = {
   explorerUrlToTx: "https://dev-explorer.comdex.one/transactions/{txHash}",
   apiUrl: "https://testnet-stat.comdex.one",
   comdexStakingUrl: "https://comdex.omniflix.co/stake",
-  WebSocketApiUrl: "wss://rpc.comdex.one/websocket",
+  webSocketApiUrl: "wss://rpc.comdex.one/websocket",
 
   commodo: {
-    title: "Commodo",
-    websiteUrl: "https://devnet.commodo.one",
     appId: 3,
     cswapAppId: 2,
     numberOfTopAssets: 3,
@@ -23,10 +21,8 @@ export const envConfig = {
     rewardsUrl: "https://docs.commodo.one/rewards",
     atomCmdxPoolId: 1,
   },
-  
+
   cSwap: {
-    title: "cSwap Exchange",
-    websiteUrl: "https://devnet.cswap.one",
     appId: 1,
     masterPoolId: 1,
     networkTag: "Devnet",
