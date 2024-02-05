c72f6ea3cbcaff500e347238cfed46357ccfa87c
Chaitanya Prabhu <chaitanya@Chaitanyas-MacBook-Pro.local>
2024-02-05 17:39:15 +0530

    updated devnet chain-id

--- devnet/envConfig.js
+++ devnet/envConfig.js
@@ -1,7 +1,7 @@
 export const envConfig = {
   rpc: "https://devnet.rpc.comdex.one",
   rest: "https://devnet.rest.comdex.one",
-  chainId: "test-4",
+  chainId: "test-1",
   coinDenom: "CMDX",
   coinMinimalDenom: "ucmdx",
   coinDecimals: 6,
